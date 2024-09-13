13546006957b16ec7750791e8993135a5fa10185
AAlonsoL <adrian.alonso.ledesma0@gmail.com>
2024-09-13 19:24:30 +0200

    User #1

---
+++ src/main/java/es/upm/miw/iwvg_devops/models/User.java
@@ -0,0 +1,27 @@
+package es.upm.miw.iwvg_devops.models;
+
+public class User{
+
+    private int id;
+    private int edad;
+
+    public User(int id, int edad) {
+        this.id= id;
+        this.edad= edad;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
